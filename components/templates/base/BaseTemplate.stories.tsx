5f045b0f3b9affeef211b7ddac9332126ca8f98b
neb03x <neb.3391@gmail.com>
2022-12-18 11:32:12 +0530

    build: create BaseTemplate component

---
+++ components/templates/base/BaseTemplate.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import BaseTemplate, { IBaseTemplate } from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
+
+export default {
+  title: 'template/BaseTemplate',
+  component: BaseTemplate,
+  argTypes: {},
+} as ComponentMeta<typeof BaseTemplate>;
+
+const Template: ComponentStory<typeof BaseTemplate> = (args) => (
+  <BaseTemplate {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockBaseTemplateProps.base,
+} as IBaseTemplate;
